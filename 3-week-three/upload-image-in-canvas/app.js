9d6852343ea43630a499e86ad23e3fbad7dc3ff7
Sherief Elnahas <sheriefelnahas5@gmail.com>
2023-04-10 01:08:53 +0200

    upload image in canvas exercise

---
+++ 3-week-three/upload-image-in-canvas/app.js
@@ -0,0 +1,12 @@
+
+
+function upload() {
+  const imgCanvas = document.querySelector('canvas');
+  const fileInput = document.querySelector('#finput');
+  const image = new SimpleImage(fileInput);
+  
+  image.drawTo(imgCanvas);
+
+
+
+}
