248d09ca0eccffb007160f0ed239e96042ed6651
Sherief Elnahas <sheriefelnahas5@gmail.com>
2023-04-10 02:52:12 +0200

    modifiy upload image to canvas

---
+++ 3-week-three/convert-image-to-gray/app.js
@@ -0,0 +1,28 @@
+let image;
+const originalCanvas = document.querySelector('#originalCanvas');
+const grayCanavas = document.querySelector('#grayCanvas')
+
+function upload() {
+
+  const fileInput = document.querySelector('#finput');
+  image = new SimpleImage(fileInput);
+
+  image.drawTo(originalCanvas);
+}
+
+function makeGray() {
+  if(image) {
+    for(let pixel of image.values()) {
+      const red  = pixel.getRed();
+      const green = pixel.getGreen();
+      const blue = pixel.getBlue();
+      const avg = ((red + green + blue) / 3)
+
+      pixel.setRed(avg);
+      pixel.setGreen(avg);
+      pixel.setBlue(avg);
+    }
+  }
+  image.drawTo(grayCanavas);
+}
+
